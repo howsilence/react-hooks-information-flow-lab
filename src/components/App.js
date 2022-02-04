5a42d8a58d24d41618423a1440b5bc65a6f0cb2d
howsilence <statton0104@gmail.com>
2022-02-03 21:52:51 -0500

    completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,23 @@
 import React, { useState } from "react";
 import ShoppingList from "./ShoppingList";
 import itemData from "../data/items";
-
+import Header from "./Header"
 function App() {
+  
+  //state for light mode in the app
   const [isDarkMode, setIsDarkMode] = useState(false);
 
-  function handleDarkModeClick() {
+  // change the function to "on" dark mode click because this is the action we want our passed down function to take
+  function onDarkModeClick() {
     setIsDarkMode((isDarkMode) => !isDarkMode);
   }
 
   return (
     <div className={"App " + (isDarkMode ? "dark" : "light")}>
-      <header>
-        <h2>Shopster</h2>
-        <button onClick={handleDarkModeClick}>
-          {isDarkMode ? "Dark" : "Light"} Mode
-        </button>
-      </header>
+     <Header // passed down function
+     onDarkModeClick={onDarkModeClick}
+     isDarkMode={isDarkMode}
+        />
       <ShoppingList items={itemData} />
     </div>
   );
